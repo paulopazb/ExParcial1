176d7d940129d55d83114d8454e14a1974b6d321
Paulo <44127888+paulopazb@users.noreply.github.com>
2019-09-28 09:37:23 -0400

    Actualizacin datacontext

--- Becerra/Cesar/Models/DataContext.cs
+++ Becerra/Cesar/Models/DataContext.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿
 
 namespace Cesar.Models
 {
+    using System.Data.Entity;
     public class DataContext:DbContext
     {
         public DataContext():base("DefaultConnection")
