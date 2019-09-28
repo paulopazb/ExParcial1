bd4faf3b0a7382100d9de690d871c038d0eccd0e
Paulo <44127888+paulopazb@users.noreply.github.com>
2019-09-28 09:32:23 -0400

    Creacion cadena de conexion y pruba de bd

--- Becerra/Cesar/Models/Fairy.cs
+++ Becerra/Cesar/Models/Fairy.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿
 
 namespace Cesar.Models
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
     public class Fairy
-    {
+    { 
         public enum TypeWishes
         {
             Pelicula,
